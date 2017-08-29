bb8bf251f3d0e5d14d36b5a986cad6903cc1b666
jameshloving <jameshloving@gmail.com>
2017-08-28 21:14:57 -0400

    Fix str_time typo, add get_time_list()

--- source/utils.py
+++ source/utils.py
@@ -1,3 +1,7 @@
+import datetime
+import googlemaps
+from IPython import embed
+
 def import_API_key(api_key_file):
     """
     !@brief Import a Google Maps API key from a file
@@ -36,8 +40,19 @@ def str_time_to_int_minutes(str_time):
     @return Integer number of minutes in time
     """
 
-    if "hour" not in string:
+    if "hour" not in str_time:
         return int(str_time.split()[0])
 
     else:
         return int(str_time.split()[0])*60 + int(str_time.split()[2])
+
+
+
+def get_time_list(gmaps, src_address, dst_address):
+    time = datetime.datetime.now() + datetime.timedelta(days = (7 - datetime.datetime.now().weekday()))
+    travel_times = {}
+    for i in range(12):
+        time = time.replace(hour = 6 + i)
+        travel_times[str(6 + i)] = str_time_to_int_minutes(gmaps.distance_matrix(src_address, dst_address, departure_time=time)['rows'][0]['elements'][0]['duration_in_traffic']['text'])
+
+    return travel_times
